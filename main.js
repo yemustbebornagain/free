8e31b0ed6870f04227b32cddb9f703c3609d317c
getstudentcard <119403855+getstudentcard@users.noreply.github.com>
2022-11-29 10:38:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 $(document).ready(function () {
-    $('iframe').on('load', function() {
-        $('iframe').contents().find("head").append("<style>.topbanner-wrapper-desktop{display:none;}</style>");
-    }); 
+
 });
