bca0aa496ebd88225e0cbe41eb32d4b4c17a46b7
getstudentcard <119403855+getstudentcard@users.noreply.github.com>
2022-11-29 13:08:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-$(document).ready(function () {
+var editProfileWindowShow = false;
 
+$(document).ready(function () {
+    $("#edit-profile-overlay").click(function() {
+        editProfileWindowShow = !editProfileWindowShow;
+        if (editProfileWindowShow) {
+            $("#edit-profile").show();
+        } else {
+            $("#edit-profile").hide();
+        }
+    });
 });
